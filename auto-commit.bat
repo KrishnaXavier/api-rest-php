8bd2dbe0ea5a323d55c9f143fa6299003f88c68e
Krishna Xavier <tsixav@gmail.com>
2018-03-02 22:44:29 -0300

    Redirecionar todos para index.php

---
+++ auto-commit.bat
@@ -0,0 +1,8 @@
+REM[source project: https://github.com/KrishnaXavier/auto-commit]
+git add .
+
+git status
+
+git commit -a -m %1
+
+git push origin master
---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>APIT REST - PHP</title>
+</head>
+<body>
+
+<?php var_dump($_SERVER); ?>
+
+</body>
+</html>
