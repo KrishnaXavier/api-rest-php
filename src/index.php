387c7492e5e0e27c6940c0de99c3e498c2407dab
KrishnaXavier <tsixav@gmail.com>
2018-03-02 22:49:06 -0300

    cdn jquery/ajax

--- src/index.php
+++ src/index.php
@@ -2,10 +2,14 @@
 <html>
 <head>
 	<title>APIT REST - PHP</title>
+	<script
+	src="https://code.jquery.com/jquery-3.3.0.min.js"
+	integrity="sha256-RTQy8VOmNlT6b2PIRur37p6JEBZUE7o8wPgMvu18MC4="
+	crossorigin="anonymous"></script>
 </head>
 <body>
 
-<?php var_dump($_SERVER); ?>
+	<?php var_dump($_SERVER); ?>
 
 </body>
 </html>
