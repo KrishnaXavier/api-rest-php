2bdf04b99cd1a5aaf1530d7f4a7d4f9a464eade5
KrishnaXavier <tsixav@gmail.com>
2018-03-03 10:29:50 -0300

    Pegando a classe e valor da query string

--- src/request.php
+++ src/request.php
@@ -5,6 +5,16 @@
 	<script src="https://code.jquery.com/jquery-3.3.0.min.js" integrity="sha256-RTQy8VOmNlT6b2PIRur37p6JEBZUE7o8wPgMvu18MC4=" crossorigin="anonymous"></script>
 </head>
 <body>
+	
+	<div>
+		<h3>Use request(query, method, data) in console. </h3>		
+	</div>
+
+	<div>
+		Example: 
+		<div>request('produto/10', 'GET', {})</div>
+	</div>
+
 </body>
 <script type="text/javascript">
 	function request(query, method, data){
--- src/response.php
+++ src/response.php
@@ -1,8 +1,21 @@
-<?php 
-//var_dump($_SERVER);
+<?php
+
+/* config */
+$validURLs = ['produto'];
 
 $request = [];
 $request['method'] = $_SERVER['REQUEST_METHOD'];
 $request['queryString'] = $_SERVER['QUERY_STRING'];
+
+$query = explode("/", $request['queryString']);
+foreach ($query as $key => $value) {
+	
+	if(in_array($value, $validURLs)){
+		$request['class'] = $value;
+		$request['id'] = isset($query[$key+1]) ? $query[$key+1] : "all";		
+		break;
+	}
+		
+}
 
 echo json_encode($request);
